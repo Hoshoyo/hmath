240cf116b2dfecf5a291a8369f3cb673385a6642
Hoshoyo <hoshoyo@gmail.com>
2022-01-29 13:42:38 -0300

    Add dvec4, vec3 and vec2 functions

--- examples/main.c
+++ examples/main.c
@@ -1,7 +1,66 @@
 #include <stdio.h>
+#include <assert.h>
+#include <math.h>
 #include "hmath.h"
+
+#define WITHIN_MARGIN(F, G, M) (((F) >= ((G) - (M))) && ((F) <= ((G) + (M))))
+
+void test_vec4()
+{
+    vec4 v1 = (vec4){1.0f, 2.0f, 3.0f, 4.0f};
+    vec4 v2 = (vec4){5.0f, 6.0f, 7.0f, 8.0f};
+    vec4 v3 = (vec4){-5.0f, -6.0f, 7.0f, 8.0f};
+    float s1 = 3.0f;
+    float v1_len = sqrtf(v1.x * v1.x + v1.y * v1.y + v1.z * v1.z + v1.w * v1.w);
+
+    assert(hm_vec4_equal(v1, v2) == 0);
+    assert(hm_vec4_equal(v1, v1) != 0);
+
+    vec4 radd = hm_vec4_add(&v1, &v2);
+    assert(radd.x == (v1.x + v2.x) && radd.y == (v1.y + v2.y) && radd.z == (v1.z + v2.z) && radd.w == (v1.w + v2.w));
+    vec4 rsub = hm_vec4_sub(&v1, &v2);
+    assert(rsub.x == (v1.x - v2.x) && rsub.y == (v1.y - v2.y) && rsub.z == (v1.z - v2.z) && rsub.w == (v1.w - v2.w));
+
+    float rdot = hm_vec4_dot(&v1, &v2);
+    assert(rdot == (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z + v1.w * v2.w));
+
+    vec4 rcross = hm_vec4_cross(&v1, &v2);
+    assert( (rcross.x == v1.y * v2.z - v1.z * v2.y) &&
+            (rcross.y == v1.z * v2.x - v1.x * v2.z) &&
+            (rcross.z == v1.x * v2.y - v1.y * v2.x) &&
+            (rcross.w == 0.0f));
+    
+    vec4 rsmul = hm_vec4_scalar_mul(&v1, s1);
+    assert(rsmul.x == v1.x * s1);
+
+    vec4 rabs = hm_vec4_abs(&v3);
+    assert(rabs.x == fabsf(rabs.x) && rabs.y == fabsf(rabs.y) && rabs.z == fabsf(rabs.z) && rabs.w == fabsf(rabs.w));
+
+    vec4 rnorm = hm_vec4_normalize(&v1);
+    assert(rnorm.x == v1.x / v1_len && rnorm.y == v1.y / v1_len && rnorm.z == v1.z / v1_len && rnorm.w == v1.w / v1_len);
+    
+    vec4 rnormfast = hm_vec4_normalize_fast(&v1);
+    assert(WITHIN_MARGIN(rnormfast.x, v1.x / v1_len, 0.0001f) && 
+        WITHIN_MARGIN(rnormfast.y, v1.y / v1_len, 0.0001f) && 
+        WITHIN_MARGIN(rnormfast.z, v1.z / v1_len, 0.0001f) && 
+        WITHIN_MARGIN(rnormfast.w, v1.w / v1_len, 0.0001f));
+
+    float len = hm_vec4_length(&v1);
+    assert(len == v1_len); 
+}
+
+void test_vec4d()
+{
+    
+}
+
+void test_mat4()
+{
+
+}
 
 int main(int argc, char** argv)
 {
+    test_vec4();
     return 0;
 }
--- hmath.h
+++ hmath.h
@@ -97,15 +97,16 @@ inline float hm_vec4_dot(vec4* restrict v1, vec4* restrict v2)
 
 inline vec4 hm_vec4_cross(vec4* restrict v1, vec4* restrict v2)
 {
-	__m128 a1 = _mm_load_ps((const float*)v1);
-	__m128 a2 = _mm_load_ps((const float*)v2);
-	__m128 a = _mm_permute_ps(a1, _MM_PERM_DACB);
-	__m128 b = _mm_permute_ps(a2, _MM_PERM_DBAC);
-	__m128 c = _mm_permute_ps(a1, _MM_PERM_DBAC);
-	__m128 d = _mm_permute_ps(a2, _MM_PERM_DACB);
+	__m128 a = _mm_load_ps((const float*)v1);
+	__m128 b = _mm_load_ps((const float*)v2);
 
-	__m128 e = _mm_mul_ps(a, b);
-	__m128 f = _mm_mul_ps(c, d);
+	__m128 aa = _mm_permute_ps(a, _MM_PERM_DACB);
+	__m128 bb = _mm_permute_ps(b, _MM_PERM_DBAC);
+	__m128 cc = _mm_permute_ps(a, _MM_PERM_DBAC);
+	__m128 dd = _mm_permute_ps(b, _MM_PERM_DACB);
+
+	__m128 e = _mm_mul_ps(aa, bb);
+	__m128 f = _mm_mul_ps(cc, dd);
 
 	return *(vec4*)_mm_sub_ps(e, f).m128_f32;
 }
@@ -121,34 +122,335 @@ inline vec4 hm_vec4_scalar_mul(vec4* restrict v, float s)
 inline vec4 hm_vec4_abs(vec4* restrict v)
 {
 	__m128 minus_two = _mm_set1_ps(-2.0f);
-	__m128 constant = _mm_set1_ps(0);
+	__m128 zero = _mm_set1_ps(0);	
 	__m128 a = _mm_load_ps((const float*)v);
-	__m128 b = _mm_cmplt_ps(a, constant);
-	__m128 mask = _mm_add_ps(_mm_and_ps(minus_two, b), _mm_set_ps(1.0f, 1.0f, 1.0f, 1.0f));
+
+	__m128 comp = _mm_cmplt_ps(a, zero);
+	__m128 mask = _mm_add_ps(_mm_and_ps(minus_two, comp), _mm_set_ps(1.0f, 1.0f, 1.0f, 1.0f));
+	
 	return *(vec4*)_mm_mul_ps(a, mask).m128_f32;
+}
+
+inline vec4 hm_vec4_normalize_fast(vec4* restrict v)
+{
+	__m128 a = _mm_load_ps((const float*)v);
+	__m128 dot = _mm_dp_ps(a, a, 0xff);
+	__m128 rsqroot = _mm_rsqrt_ps(dot);
+
+	return *(vec4*)_mm_mul_ps(a, rsqroot).m128_f32;
 }
 
 inline vec4 hm_vec4_normalize(vec4* restrict v)
 {
 	__m128 a = _mm_load_ps((const float*)v);
-	__m128 b = _mm_dp_ps(a, a, 0xff);
-	__m128 c = _mm_rsqrt_ps(b);
+	__m128 dot = _mm_dp_ps(a, a, 0xff);
+	__m128 sqroot = _mm_sqrt_ps(dot);
 
-	return *(vec4*)_mm_mul_ps(c, a).m128_f32;
+	return *(vec4*)_mm_div_ps(a, sqroot).m128_f32;
 }
 
 inline float hm_vec4_length(vec4* restrict v)
 {
 	__m128 a = _mm_load_ps((const float*)v);
-	__m128 b = _mm_dp_ps(a, a, 0xff);
-	return _mm_sqrt_ps(b).m128_f32[0];
+	__m128 dot = _mm_dp_ps(a, a, 0xff);
+
+	return _mm_sqrt_ps(dot).m128_f32[0];
 }
 
 /* vec3 functions */
+inline int hm_vec3_equal(vec3 v1, vec3 v2)
+{
+	return (*(unsigned long long*)&v1.x == *(unsigned long long*)&v2.x) && (v1.z == v2.z);
+}
+
+inline vec3 hm_vec3_add(vec3* restrict v1, vec3* restrict v2)
+{
+	__m128i mask = _mm_set_epi32(0, 0xffffffff, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v1, mask);
+	__m128 b = _mm_maskload_ps((const float*)v2, mask);
+
+	return *(vec3*)_mm_add_ps(a, b).m128_f32;
+}
+
+inline vec3 hm_vec3_sub(vec3* restrict v1, vec3* restrict v2)
+{
+	__m128i mask = _mm_set_epi32(0, 0xffffffff, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v1, mask);
+	__m128 b = _mm_maskload_ps((const float*)v2, mask);
+
+	return *(vec3*)_mm_sub_ps(a, b).m128_f32;
+}
+
+inline float hm_vec3_dot(vec3* restrict v1, vec3* restrict v2)
+{
+	__m128i mask = _mm_set_epi32(0, 0xffffffff, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v1, mask);
+	__m128 b = _mm_maskload_ps((const float*)v2, mask);
+
+	return _mm_dp_ps(a, b, 0xff).m128_f32[0];
+}
+
+inline vec3 hm_vec3_cross(vec3* restrict v1, vec3* restrict v2)
+{
+	__m128i mask = _mm_set_epi32(0, 0xffffffff, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v1, mask);
+	__m128 b = _mm_maskload_ps((const float*)v2, mask);
+
+	__m128 aa = _mm_permute_ps(a, _MM_PERM_DACB);
+	__m128 bb = _mm_permute_ps(b, _MM_PERM_DBAC);
+	__m128 cc = _mm_permute_ps(a, _MM_PERM_DBAC);
+	__m128 dd = _mm_permute_ps(b, _MM_PERM_DACB);
+
+	__m128 e = _mm_mul_ps(aa, bb);
+	__m128 f = _mm_mul_ps(cc, dd);
+
+	return *(vec3*)_mm_sub_ps(e, f).m128_f32;
+}
+
+inline vec3 hm_vec3_scalar_mul(vec3* restrict v, float s)
+{
+	__m128 scalar = _mm_set_ps(s, s, s, s);
+	__m128i mask = _mm_set_epi32(0, 0xffffffff, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v, mask);
+
+	return *(vec3*)_mm_mul_ps(a, scalar).m128_f32;
+}
+
+inline vec3 hm_vec3_abs(vec3* restrict v)
+{
+	__m128 minus_two = _mm_set1_ps(-2.0f);
+	__m128 zero = _mm_set1_ps(0);	
+	__m128i loadmask = _mm_set_epi32(0, 0xffffffff, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v, loadmask);
+
+	__m128 comp = _mm_cmplt_ps(a, zero);
+	__m128 mask = _mm_add_ps(_mm_and_ps(minus_two, comp), _mm_set_ps(1.0f, 1.0f, 1.0f, 1.0f));
+	
+	return *(vec3*)_mm_mul_ps(a, mask).m128_f32;
+}
+
+inline vec3 hm_vec3_normalize_fast(vec3* restrict v)
+{
+	__m128i loadmask = _mm_set_epi32(0, 0xffffffff, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v, loadmask);
+	__m128 dot = _mm_dp_ps(a, a, 0xff);
+	__m128 rsqroot = _mm_rsqrt_ps(dot);
+
+	return *(vec3*)_mm_mul_ps(a, rsqroot).m128_f32;
+}
+
+inline vec3 hm_vec3_normalize(vec3* restrict v)
+{
+	__m128i loadmask = _mm_set_epi32(0, 0xffffffff, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v, loadmask);
+	__m128 dot = _mm_dp_ps(a, a, 0xff);
+	__m128 sqroot = _mm_sqrt_ps(dot);
+
+	return *(vec3*)_mm_div_ps(a, sqroot).m128_f32;
+}
+
+inline float hm_vec3_length(vec3* restrict v)
+{
+	__m128i loadmask = _mm_set_epi32(0, 0xffffffff, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v, loadmask);
+	__m128 dot = _mm_dp_ps(a, a, 0xff);
+
+	return _mm_sqrt_ps(dot).m128_f32[0];
+}
 
 /* vec2 functions */
+inline int hm_vec2_equal(vec2 v1, vec2 v2)
+{
+	return (*(unsigned long long*)&v1.x == *(unsigned long long*)&v2.x);
+}
+
+inline vec2 hm_vec2_add(vec2* restrict v1, vec2* restrict v2)
+{
+	__m128i mask = _mm_set_epi32(0, 0, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v1, mask);
+	__m128 b = _mm_maskload_ps((const float*)v2, mask);
+
+	return *(vec2*)_mm_add_ps(a, b).m128_f32;
+}
+
+inline vec2 hm_vec2_sub(vec2* restrict v1, vec2* restrict v2)
+{
+	__m128i mask = _mm_set_epi32(0, 0, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v1, mask);
+	__m128 b = _mm_maskload_ps((const float*)v2, mask);
+
+	return *(vec2*)_mm_sub_ps(a, b).m128_f32;
+}
+
+inline float hm_vec2_dot(vec2* restrict v1, vec2* restrict v2)
+{
+	__m128i mask = _mm_set_epi32(0, 0, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v1, mask);
+	__m128 b = _mm_maskload_ps((const float*)v2, mask);
+
+	return _mm_dp_ps(a, b, 0xff).m128_f32[0];
+}
+
+inline vec2 hm_vec2_cross(vec2* restrict v1, vec2* restrict v2)
+{
+	__m128i mask = _mm_set_epi32(0, 0, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v1, mask);
+	__m128 b = _mm_maskload_ps((const float*)v2, mask);
+
+	__m128 aa = _mm_permute_ps(a, _MM_PERM_DACB);
+	__m128 bb = _mm_permute_ps(b, _MM_PERM_DBAC);
+	__m128 cc = _mm_permute_ps(a, _MM_PERM_DBAC);
+	__m128 dd = _mm_permute_ps(b, _MM_PERM_DACB);
+
+	__m128 e = _mm_mul_ps(aa, bb);
+	__m128 f = _mm_mul_ps(cc, dd);
+
+	return *(vec2*)_mm_sub_ps(e, f).m128_f32;
+}
+
+inline vec2 hm_vec2_scalar_mul(vec2* restrict v, float s)
+{
+	__m128 scalar = _mm_set_ps(s, s, s, s);
+	__m128i mask = _mm_set_epi32(0, 0, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v, mask);
+
+	return *(vec2*)_mm_mul_ps(a, scalar).m128_f32;
+}
+
+inline vec2 hm_vec2_abs(vec2* restrict v)
+{
+	__m128 minus_two = _mm_set1_ps(-2.0f);
+	__m128 zero = _mm_set1_ps(0);	
+	__m128i loadmask = _mm_set_epi32(0, 0, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v, loadmask);
+
+	__m128 comp = _mm_cmplt_ps(a, zero);
+	__m128 mask = _mm_add_ps(_mm_and_ps(minus_two, comp), _mm_set_ps(1.0f, 1.0f, 1.0f, 1.0f));
+	
+	return *(vec2*)_mm_mul_ps(a, mask).m128_f32;
+}
+
+inline vec2 hm_vec2_normalize_fast(vec2* restrict v)
+{
+	__m128i loadmask = _mm_set_epi32(0, 0, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v, loadmask);
+	__m128 dot = _mm_dp_ps(a, a, 0xff);
+	__m128 rsqroot = _mm_rsqrt_ps(dot);
+
+	return *(vec2*)_mm_mul_ps(a, rsqroot).m128_f32;
+}
+
+inline vec2 hm_vec2_normalize(vec2* restrict v)
+{
+	__m128i loadmask = _mm_set_epi32(0, 0, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v, loadmask);
+	__m128 dot = _mm_dp_ps(a, a, 0xff);
+	__m128 sqroot = _mm_sqrt_ps(dot);
+
+	return *(vec2*)_mm_div_ps(a, sqroot).m128_f32;
+}
+
+inline float hm_vec2_length(vec2* restrict v)
+{
+	__m128i loadmask = _mm_set_epi32(0, 0, 0xffffffff, 0xffffffff);
+	__m128 a = _mm_maskload_ps((const float*)v, loadmask);
+	__m128 dot = _mm_dp_ps(a, a, 0xff);
+
+	return _mm_sqrt_ps(dot).m128_f32[0];
+}
 
 /* dvec4 functions */
+inline int hm_dvec4_equal(dvec4 v1, dvec4 v2)
+{
+	return (*(unsigned long long*)&v1.x == *(unsigned long long*)&v2.x) && 
+		   (*(unsigned long long*)&v1.y == *(unsigned long long*)&v2.y) &&
+		   (*(unsigned long long*)&v1.z == *(unsigned long long*)&v2.z) &&
+		   (*(unsigned long long*)&v1.w == *(unsigned long long*)&v2.w);
+}
+
+inline dvec4 hm_dvec4_add(dvec4* restrict v1, dvec4* restrict v2)
+{
+	__m256d a = _mm256_load_pd((const double*)v1);
+	__m256d b = _mm256_load_pd((const double*)v2);
+	return *(dvec4*)_mm256_add_pd(a, b).m256d_f64;
+}
+
+inline dvec4 hm_dvec4_sub(dvec4* restrict v1, dvec4* restrict v2)
+{
+	__m256d a = _mm256_load_pd((const double*)v1);
+	__m256d b = _mm256_load_pd((const double*)v2);
+	return *(dvec4*)_mm256_sub_pd(a, b).m256d_f64;
+}
+
+inline double hm_dvec4_dot(dvec4* restrict v1, dvec4* restrict v2)
+{
+	__m256d a = _mm256_load_pd((const double*)v1);
+	__m256d b = _mm256_load_pd((const double*)v2);
+
+	__m256d m = _mm256_mul_pd(a, b);
+	__m256d h = _mm256_hadd_pd(m, m);
+
+	return h.m256d_f64[0] + h.m256d_f64[2];
+}
+
+inline dvec4 hm_dvec4_cross(dvec4* restrict v1, dvec4* restrict v2)
+{
+	__m256d a = _mm256_load_pd((const double*)v1);
+	__m256d b = _mm256_load_pd((const double*)v2);
+
+	__m256d aa = _mm256_permute4x64_pd(a, _MM_PERM_DACB);
+	__m256d bb = _mm256_permute4x64_pd(b, _MM_PERM_DBAC);
+	__m256d cc = _mm256_permute4x64_pd(a, _MM_PERM_DBAC);
+	__m256d dd = _mm256_permute4x64_pd(b, _MM_PERM_DACB);
+
+	__m256d e = _mm256_mul_pd(aa, bb);
+	__m256d f = _mm256_mul_pd(cc, dd);
+
+	return *(dvec4*)_mm256_sub_pd(e, f).m256d_f64;
+}
+
+inline dvec4 hm_dvec4_scalar_mul(dvec4* restrict v, double s)
+{
+	__m256d scalar = _mm256_set_pd(s, s, s, s);
+	__m256d a = _mm256_load_pd((const double*)v);
+
+	return *(dvec4*)_mm256_mul_pd(a, scalar).m256d_f64;
+}
+
+inline dvec4 hm_dvec4_abs(dvec4* restrict v)
+{
+	__m256d minus_two = _mm256_set1_pd(-2.0);
+	__m256d zero = _mm256_set1_pd(0);
+	__m256d a = _mm256_load_pd((const double*)v);
+
+	__m256d comp = _mm256_cmp_pd(a, zero, _CMP_LT_OS);
+	__m256d mask = _mm256_add_pd(_mm256_and_pd(minus_two, comp), _mm256_set_pd(1.0, 1.0, 1.0, 1.0));
+
+	return *(dvec4*)_mm256_mul_pd(a, mask).m256d_f64;
+}
+
+inline dvec4 hm_dvec4_normalize(dvec4* restrict v)
+{
+	__m256d a = _mm256_load_pd((const double*)v);
+
+	__m256d aa = _mm256_mul_pd(a, a);
+	__m256d hadd = _mm256_hadd_pd(aa, aa);
+	__m256d dot = _mm256_set1_pd(hadd.m256d_f64[1] + hadd.m256d_f64[3]);
+	__m256d len = _mm256_sqrt_pd(dot);
+
+	return *(dvec4*)_mm256_div_pd(a, len).m256d_f64;
+}
+
+inline double hm_dvec4_length(dvec4* restrict v)
+{
+	__m256d a = _mm256_load_pd((const double*)v);
+
+	__m256d aa = _mm256_mul_pd(a, a);
+	__m256d hadd = _mm256_hadd_pd(aa, aa);
+	__m256d dot = _mm256_set1_pd(hadd.m256d_f64[1] + hadd.m256d_f64[3]);
+	
+	return _mm256_sqrt_pd(dot).m256d_f64[0];
+}
 
 /* dvec3 functions */
 
@@ -171,6 +473,26 @@ inline vec4 hm_mat4_multiply_vec4(mat4* restrict m, vec4* restrict v)
 	__m128 result = _mm_shuffle_ps(_mm_movelh_ps(prod1, prod2), _mm_movelh_ps(prod3, prod4), _MM_SHUFFLE(2, 0, 2, 0));
 
 	return *(vec4*)result.m128_f32;
+}
+
+inline vec3 sm_mat4_multiply_vec3(mat4* restrict m, vec3* restrict v)
+{
+	__m128i mask = _mm_set_epi32(0, 0xffffffff, 0xffffffff, 0xffffffff);
+	__m128 vec = _mm_maskload_ps((const float*)v, mask);
+	vec.m128_f32[3] = 1.0f;
+	__m128 r1 = _mm_load_ps((const float*)&m->data[0][0]);
+	__m128 r2 = _mm_load_ps((const float*)&m->data[1][0]);
+	__m128 r3 = _mm_load_ps((const float*)&m->data[2][0]);
+	__m128 r4 = _mm_load_ps((const float*)&m->data[3][0]);
+
+	__m128 prod1 = _mm_dp_ps(r1, vec, 0xFF);
+	__m128 prod2 = _mm_dp_ps(r2, vec, 0xFF);
+	__m128 prod3 = _mm_dp_ps(r3, vec, 0xFF);
+	__m128 prod4 = _mm_dp_ps(r4, vec, 0xFF);
+
+	__m128 result = _mm_shuffle_ps(_mm_movelh_ps(prod1, prod2), _mm_movelh_ps(prod3, prod4), _MM_SHUFFLE(2, 0, 2, 0));
+
+	return *(vec3*)result.m128_f32;
 }
 
 inline mat4 hm_mat4_multiply(mat4* restrict m1, mat4* restrict m2)
